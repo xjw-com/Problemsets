7f6f33c4e28e66c03edee386948acdc56bcbf075
xjw <671173782@qq.com>
2022-07-03 15:41:28 +0800

    Initial commit

---
+++ src/main/java/Xjw/DataStructure/Controller/TableStackQueueAnswer.java
@@ -0,0 +1,29 @@
+package Xjw.DataStructure.Controller;
+
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @Author xjw
+ * @Date 2022/7/3 15:16
+ * @Version 1.0
+ */
+public class TableStackQueueAnswer {
+    public void printLots(List<Integer> L,List<Integer> P){
+        Iterator<Integer> iteratorL=L.iterator();
+        Iterator<Integer> iteratorP=P.iterator();
+        Integer itemL=null;
+        Integer itemP;
+        int itemStart=0;
+        while (iteratorL.hasNext()&&iteratorP.hasNext()){
+            itemP=iteratorP.next();
+            while (itemStart<itemP&&iteratorL.hasNext()){
+                itemStart++;
+                itemL=iteratorL.next();
+            }
+        }
+        System.out.print(itemL+"");
+    }
+// 时间复杂度为O(N^2)|O(n*m) n,m 为L,P的长度。
+
+}
---
+++ src/main/java/Xjw/DataStructure/ProblemDescription/ProblemDescriptionTableStackQueue.java
@@ -0,0 +1,15 @@
+package Xjw.DataStructure.ProblemDescription;
+
+/**
+ * @Author xjw
+ * @Date 2022/7/3 15:03
+ * @Version 1.0
+ */
+
+public class ProblemDescriptionTableStackQueue {
+    /**
+     * 1.给定一个表L和另外一个表P,他们包含以升序排序的整数。操作printLots（L,P）将打印L中那些由P所指定的位置上的元素。
+     * 例如，如果P=1,3,4,6,那么，L中位于第1,第3，第4和第6个位置上的元素被打印出来。写出过程printLots(L,P)。
+     * 只可使用public型的Collection API容器操作。该过程的运行的时间是多少？
+     */
+}
