4048d75858a04c2e2db0134e4259c90f1904e80d
肖杰文 <671173782@qq.com>
2022-07-05 22:06:08 +0800

    例题2提交

--- src/main/java/Xjw/DataStructure/Controller/Answer02.java
+++ src/main/java/Xjw/DataStructure/Controller/Answer02.java
@@ -6,13 +6,83 @@ public class Answer02 {
         private Integer value;
         private singleLinkNode next;
 
-        public singleLinkNode(Integer V, singleLinkNode N) {
+        public singleLinkNode(Integer V) {
             value = V;
-            next = N;
+            next=null;
         }
-
         public Integer getValue() {
             return value;
         }
     }
+    public class singleList{
+        public  singleLinkNode singleLinkNodeHead;
+
+        //得到单链表的长度
+        public int size(){
+            singleLinkNode singleLinkNodeLen=singleLinkNodeHead;
+            int size=0;
+            while (singleLinkNodeLen!=null){
+                size++;
+                singleLinkNodeLen=singleLinkNodeLen.next;
+            }
+            return size;
+        }
+        //根据索引找到节点的位置
+        private singleLinkNode getNodeIndex(int index){
+            if(index<0||index>size()){
+                System.out.println("索引index的位置不合法");
+            }
+            singleLinkNode node=singleLinkNodeHead;
+            while (index-1!=0){
+                //节点向后遍历一位，索引减一。当索引等于一时找到节点
+                node=node.next;
+                index--;
+            }
+            return node;
+        }
+
+        //在头部插入数据
+        public void addHeadData(int data){
+            singleLinkNode singleLinkNodeAddHead=new singleLinkNode(data);
+            if(singleLinkNodeHead==null){
+                singleLinkNodeHead=singleLinkNodeAddHead;
+                return;
+            }
+            singleLinkNodeAddHead.next=singleLinkNodeHead;
+            singleLinkNodeHead=singleLinkNodeAddHead;
+        }
+        // 在尾部进行插入
+        public void addTailData(int data){
+            singleLinkNode singleLinkNodeAddTail=new singleLinkNode(data);
+            if(singleLinkNodeHead==null){
+                singleLinkNodeHead=singleLinkNodeAddTail;
+                return;
+            }
+            singleLinkNode curNode=singleLinkNodeHead;
+            while (curNode.next!=null){
+                curNode=curNode.next;
+            }
+            curNode.next=singleLinkNodeAddTail;
+        }
+
+        //按索引进行插入
+        public void addIndex(int index,int data){
+            singleLinkNode singleLikNodeAdd=new singleLinkNode(data);
+            if(index==0){
+                addHeadData(data);
+            }
+            if(index==size()){
+                addTailData(data);
+            }
+            singleLinkNode addNode=getNodeIndex(index);
+            //顺序不能反。
+            singleLikNodeAdd.next=addNode.next;
+            addNode.next=singleLikNodeAdd;
+        }
+
+        //交换两相邻的元素--指定元素和它后一个节点交换。
+
+
+    }
+
 }
