4f9df73c845fab13e45ebe3da39eeb0ede9bcc99
肖杰文 <671173782@qq.com>
2022-07-06 22:11:03 +0800

    例题2提交

--- src/main/java/Xjw/DataStructure/Controller/Answer02.java
+++ src/main/java/Xjw/DataStructure/Controller/Answer02.java
@@ -10,15 +10,12 @@ public class Answer02 {
             value = V;
             next=null;
         }
-        public Integer getValue() {
-            return value;
-        }
     }
     public class singleList{
-        public  singleLinkNode singleLinkNodeHead;
+        private   singleLinkNode singleLinkNodeHead;
 
         //得到单链表的长度
-        public int size(){
+        private int size(){
             singleLinkNode singleLinkNodeLen=singleLinkNodeHead;
             int size=0;
             while (singleLinkNodeLen!=null){
@@ -42,7 +39,7 @@ public class Answer02 {
         }
 
         //在头部插入数据
-        public void addHeadData(int data){
+        private void addHeadData(int data){
             singleLinkNode singleLinkNodeAddHead=new singleLinkNode(data);
             if(singleLinkNodeHead==null){
                 singleLinkNodeHead=singleLinkNodeAddHead;
@@ -52,7 +49,7 @@ public class Answer02 {
             singleLinkNodeHead=singleLinkNodeAddHead;
         }
         // 在尾部进行插入
-        public void addTailData(int data){
+        private void addTailData(int data){
             singleLinkNode singleLinkNodeAddTail=new singleLinkNode(data);
             if(singleLinkNodeHead==null){
                 singleLinkNodeHead=singleLinkNodeAddTail;
@@ -64,9 +61,8 @@ public class Answer02 {
             }
             curNode.next=singleLinkNodeAddTail;
         }
-
         //按索引进行插入
-        public void addIndex(int index,int data){
+        private void addIndex(int index,int data){
             singleLinkNode singleLikNodeAdd=new singleLinkNode(data);
             if(index==0){
                 addHeadData(data);
@@ -80,9 +76,74 @@ public class Answer02 {
             addNode.next=singleLikNodeAdd;
         }
 
-        //交换两相邻的元素--指定元素和它后一个节点交换。
-
-
+        //交换两相邻的元素--指定元素和它后一个节点交换。如果为最后一个节点则和它前一个节点交换(通过调整链而不是调整数据)
+        private void changeNode(int index){
+            if(index<0||index>size()){
+                System.out.println("索引index的位置不合法");
+            }
+            if(index==size()){
+                singleLinkNode Node=getNodeIndex(index);
+                singleLinkNode preNode=getNodeIndex(index-1);
+                singleLinkNode prepreNode=getNodeIndex(index-2);
+                preNode.next=Node.next;
+                Node.next=preNode;
+                prepreNode.next=Node;
+            }else{
+                singleLinkNode Node=getNodeIndex(index);
+                singleLinkNode preNode=getNodeIndex(index-1);
+                singleLinkNode NodeNext=getNodeIndex(index+1);
+                Node.next=NodeNext.next;
+                NodeNext.next=Node;
+                preNode.next=NodeNext;
+            }
+        }
     }
+
+
+    //多链表
+    class DoubleLinkList{
+        private int value;
+        private DoubleLinkList NodePre;
+        private DoubleLinkList NodeNext;
+        public DoubleLinkList(int val){
+            value=val;
+        }
+    }
+
+    public class DoubleList{
+        private DoubleLinkList NodeHead;
+        private DoubleLinkList NodeTail;
+        // 获取size
+        public int size(){
+            int count=0;
+            DoubleLinkList curNode=NodeHead;
+            while (curNode!=null){
+                count++;
+                curNode=curNode.NodeNext;
+            }
+            return count;
+        }
+
+        private DoubleLinkList getNodeIndex(int index){
+            if(index<0||index>size()){
+                System.out.println("索引index的位置不合法");
+            }
+            DoubleLinkList curNode=NodeHead;
+            while (index-1!=0){
+                index--;
+                curNode=curNode.NodeNext;
+            }
+            return curNode;
+        }
+
+        //头部插入数据
+        private void addHead(int data){
+            DoubleLinkList addNode=new DoubleLinkList(data);
+            if(NodeHead==null){
+
+            }
+        }
+    }
+
 
 }
